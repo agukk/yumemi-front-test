8ad9ff77a5d4a1dc296c72af71ea7ec7bc65717e
Kenta Kato <01keny01@gmail.com>
2022-12-09 21:44:49 +0900

    feat: displayed checkboxes and prefectures

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <p>Hello, world!</p>;
+  const [prefectures, setPrefectures] = useState([]);
+
+  useEffect(() => {
+    // 都道府県一覧を取得する
+    axios
+      .get("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
+        headers: { "X-API-KEY": process.env.REACT_APP_RESAS_API_KEY },
+      })
+      .then((res) => {
+        setPrefectures(res.data.result)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }, []);
+    console.log(prefectures.map((prefecture) => {
+      return prefecture.prefName
+    }))
+  
+  return(
+    <>
+      <h1>yumemi-front-test</h1>
+      <h2>都道府県</h2>
+      {prefectures.map((prefecture) => {
+        return(
+          <>
+            <label>
+              <input 
+                type="checkbox"
+              />
+              {prefecture.prefName}
+            </label>
+          </>
+        );
+      })}
+    </>
+  );
 }
 
 export default App;
