67e128b244dfe7a71ab4990d992da3e946417cf2
Kenta Kato <01keny01@gmail.com>
2022-12-12 21:36:36 +0900

    add: create title component

---
+++ src/components/atoms/Title.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Title = ({ children }) => {
+    return(
+        <STitle>{ children }</STitle>
+    );
+}
+
+const STitle = styled.h1`
+    color: black;
+    text-align: center;
+`;
