457aa1f54daa031d1b88d151d6d8b108731a5983
Kenta Kato <01keny01@gmail.com>
2022-12-13 19:12:17 +0900

    add: split component

---
+++ src/hooks/useGetPrefectures.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { client } from "../api/CreateUrl";
+
+export const useGetPrefectures = () => {
+    const [prefectures, setPrefectures] = useState([]);
+
+    useEffect(() => {
+        client
+          .get("/prefectures", {
+            headers: { "X-API-KEY": process.env.REACT_APP_RESAS_API_KEY },
+          })
+          .then((res) => {
+            setPrefectures(res.data.result);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    }, []);
+
+    return {
+        prefectures
+    };
+}
